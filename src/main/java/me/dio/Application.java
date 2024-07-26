f20f9c570cc19612c9857c264969cb989dc86acc
Edmar <edmarvieira003@gmail.com>
2024-07-26 20:43:34 -0300

    mermaid diagram

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -1,13 +1,13 @@
-package me.dio.santander_dio_rest_api;
+package me.dio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SantanderDioRestApiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SantanderDioRestApiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }

50eed03e1e282c08389f5241be195d9ce4116a75
Edmar <edmarvieira003@gmail.com>
2024-07-26 20:02:40 -0300

    setup project

---
+++ src/main/java/me/dio/santander_dio_rest_api/SantanderDioRestApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.santander_dio_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDioRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDioRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/santander_dio_rest_api/SantanderDioRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander_dio_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDioRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
